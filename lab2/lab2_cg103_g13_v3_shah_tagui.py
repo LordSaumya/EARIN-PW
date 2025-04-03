134ddde6ba11a4399b3f3ca4c915dd2228389ad4
TAGUI Anas <atagui.ir2027@esaip.org>
2025-04-04 00:43:00 +0200

    Update lab2_cg103_g13_v3_shah_tagui.py

--- lab2/lab2_cg103_g13_v3_shah_tagui.py
+++ lab2/lab2_cg103_g13_v3_shah_tagui.py
@@ -1,50 +1,98 @@
-class Nim(object):
+import sys
+
+def minimax(board, depth, maximizing_player, alpha, beta):
+    if sum(board) == 0:
+        return (-1, -1) if maximizing_player else (-1, 1)
+    
+    if maximizing_player:
+        max_eval = -sys.maxsize
+        best_move = (-1, -1)
+        for i in range(len(board)):
+            if board[i] == 0:
+                continue
+            for j in range(1, board[i] + 1):
+                new_board = board[:]
+                new_board[i] -= j
+                _, eval = minimax(new_board, depth + 1, False, alpha, beta)
+                if eval > max_eval:
+                    max_eval = eval
+                    best_move = (i, j)
+                alpha = max(alpha, eval)
+                if beta <= alpha:
+                    break
+        return best_move, max_eval
+    else:
+        min_eval = sys.maxsize
+        best_move = (-1, -1)
+        for i in range(len(board)):
+            if board[i] == 0:
+                continue
+            for j in range(1, board[i] + 1):
+                new_board = board[:]
+                new_board[i] -= j
+                _, eval = minimax(new_board, depth + 1, True, alpha, beta)
+                if eval < min_eval:
+                    min_eval = eval
+                    best_move = (i, j)
+                beta = min(beta, eval)
+                if beta <= alpha:
+                    break
+        return best_move, min_eval
+
+class Nim:
     def __init__(self, board):
-        self.board = board
-
-    # Implement any additional functions needed here
-
-    def minimax(self, board, depth, maximizing_player, alpha, beta):
-        """
-        Minimax with alpha-beta pruning algorithm
-
-        Parameters:
-        - board: 1d matrix where each entry represents pile and value in the entry represents number of sticks
-        - depth: depth
-        - maximizing_player: boolean which is equal to True when the player tries to maximize the score
-        - alpha: alpha variable for pruning
-        - beta: beta variable for pruning 
-        Returns:
-        - Best value 
-        - Everything needed to identify next move
-
-        """
-        # Your code starts here
+        self.board = board[:]
+    
+    def make_move(self, pile, sticks):
+        if 0 <= pile < len(self.board) and 1 <= sticks <= self.board[pile]:
+            self.board[pile] -= sticks
+            return True
+        return False
+    
+    def is_game_over(self):
+        return all(sticks == 0 for sticks in self.board)
 
 if __name__ == "__main__":
-
-    """
-    Main game loop
-    Firtsly, allow player to choose how many piles will be in the game and number of sticks in each pile
-
-    Implement the game loop
-    """
-
     print("Starting Nim!")
-
-    # initializing size of the game board
-    ele = int(input("Input the number of piles "))
+    num_piles = int(input("Enter number of piles: "))
     piles = []
-
-    print("Input the number of sticks in each pile (separate with ENTER)")
-    for _ in range(ele):
-        piles.append(int(input()))
-
+    for i in range(num_piles):
+        while True:
+            try:
+                sticks = int(input(f"Enter sticks in pile {i}: "))
+                if sticks < 0:
+                    raise ValueError
+                piles.append(sticks)
+                break
+            except ValueError:
+                print("Invalid input. Please enter a non-negative integer.")
+    
     game = Nim(piles)
-    print("Enter the pile to remove from (starting from 0), then space followed by enter the number of sticks to remove")
-    print("The person who removes the last stick loses!")
-    print("Example: to remove from 2nd pile 3 sticks , enter 2 3")
-
-    while True:
-       print("Pile state %s" % (game.board))
-       # Your code starts here
+    
+    while not game.is_game_over():
+        print("Current board state:", game.board)
+        while True:
+            try:
+                move = input("Enter your move (pile index and number of sticks): ")
+                pile, sticks = map(int, move.split())
+                if game.make_move(pile, sticks):
+                    break
+                else:
+                    print("Invalid move. Try again.")
+            except (ValueError, IndexError):
+                print("Invalid input. Please enter valid indices and values.")
+        
+        if game.is_game_over():
+            print("You lost! AI wins.")
+            break
+        
+        print("AI is thinking...")
+        ai_move, _ = minimax(game.board, 0, True, -sys.maxsize, sys.maxsize)
+        if ai_move == (-1, -1):
+            print("AI has no valid moves left. You win!")
+            break
+        print(f"AI removes {ai_move[1]} sticks from pile {ai_move[0]}")
+        game.make_move(ai_move[0], ai_move[1])
+        
+        if game.is_game_over():
+            print("AI lost! You win.")
